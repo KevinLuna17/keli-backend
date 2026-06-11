643cd3be043c8a2d8a8c1ef90f9db1ad891872c3
kevinlu <kelugo_14@hotmail.com>
2026-06-11 14:20:57 -0400

    fix: update users schema to use snake_case for image URL field

--- src/db/schema/usersSchema.ts
+++ src/db/schema/usersSchema.ts
@@ -2,9 +2,9 @@ import { pgTable, text } from "drizzle-orm/pg-core";
 import { timestamps } from "../../lib/columns.helper";
 
 export const usersTable = pgTable("users", {
-    id: text().primaryKey(), // Clerk userID
-    email: text().notNull(),
-    name: text(),
-    imageUrl: text(),
-    ...timestamps,
+  id: text().primaryKey(), // Clerk userID
+  email: text().notNull(),
+  name: text(),
+  image_url: text(),
+  ...timestamps,
 });
